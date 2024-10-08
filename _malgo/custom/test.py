7c651e4a06d6593ee5fe5ff3eea82b22cbe5af69
malgo <malgoo.dev@gmail.com>
2024-10-08 17:19:49 +0700

    +update test with py native

--- _malgo/custom/test.py
+++ _malgo/custom/test.py
@@ -153,39 +153,51 @@ metric.process(batched_packed_results, data_samples)
 acc = metric.compute_metrics(metric.results)
 print(acc)
 
-import torch.optim as optim
-from mmengine import track_iter_progress
 
 
-device = 'cuda' # or 'cpu'
-max_epochs = 10
 
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+# Test with native pytorch
+# import torch.optim as optim
+# from mmengine import track_iter_progress
 
-for epoch in range(max_epochs):
-    model.train()
-    losses = []
-    for data_batch in track_iter_progress(train_data_loader):
-        data = model.data_preprocessor(data_batch, training=True)
-        loss_dict = model(**data, mode='loss')
-        loss = loss_dict['loss_cls']
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+# device = 'cuda' # or 'cpu'
+# max_epochs = 10
 
-        losses.append(loss.item())
+# optimizer = optim.Adam(model.parameters(), lr=0.01)
 
-    print(f'Epoch[{epoch}]: loss ', sum(losses) / len(train_data_loader))
+# for epoch in range(max_epochs):
+#     model.train()
+#     losses = []
 
-    with torch.no_grad():
-        model.eval()
-        for data_batch in track_iter_progress(val_data_loader):
-            data = model.data_preprocessor(data_batch, training=False)
-            predictions = model(**data, mode='predict')
-            data_samples = [d.to_dict() for d in predictions]
-            metric.process(data_batch, data_samples)
+#     print(type(track_iter_progress(train_data_loader)), type(train_data_loader))
 
-        acc = metric.acc = metric.compute_metrics(metric.results)
-        for name, topk in acc.items():
-            print(f'{name}: ', topk)
+#     for data_batch in train_data_loader:
+#         data = model.data_preprocessor(data_batch, training=True)
+#         loss_dict = model(**data, mode='loss')
+#         loss = loss_dict['loss_cls']
+
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+
+#         losses.append(loss.item())
+
+#     print(f'Epoch[{epoch}]: loss ', sum(losses) / len(train_data_loader))
+
+#     with torch.no_grad():
+#         model.eval()
+#         for data_batch in val_data_loader:
+#             data = model.data_preprocessor(data_batch, training=False)
+#             predictions = model(**data, mode='predict')
+#             data_samples = [d.to_dict() for d in predictions]
+#             metric.process(data_batch, data_samples)
+
+#         acc = metric.acc = metric.compute_metrics(metric.results)
+#         for name, topk in acc.items():
+#             print(f'{name}: ', topk)
+
+# 
+
+
+# Test with MMEngine
